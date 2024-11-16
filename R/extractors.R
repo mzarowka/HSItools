242988caf108d77c40bc1da5b4e0920018864e07
mzarowka <maurycy.zarczynski@outlook.com>
2024-11-16 17:31:07 +0100

    Drop x and y in extractor of series

--- R/extractors.R
+++ R/extractors.R
@@ -54,7 +54,9 @@ extract_spectral_series <- function(raster, index = NULL, calibration = NULL, ex
       # Calculate metric depths
       dplyr::mutate(
         depth.mm = calibration$distance - (.data$y * calibration$pixel_ratio),
-        tube.mm = .data$depth.mm - calibration$point_zero)
+        tube.mm = .data$depth.mm - calibration$point_zero) |>
+      # Drop x and y
+      dplyr::select(-c(.data$x, .data$y))
   }
 
   # Reset window
