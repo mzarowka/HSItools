de9ec1825747ec00329e5a93ab238138473596aa
Maurycy Żarczyński <63068977+mzarowka@users.noreply.github.com>
2025-02-04 18:05:14 +0100

    updates to savgol filter

--- R/filters.R
+++ R/filters.R
@@ -290,47 +290,86 @@ filter_median <- function(
 #' @param m return the m-th derivative of the filter coefficients.
 #' @param ts time scaling factor.
 #' @param extent an extent or SpatVector used to subset SpatRaster. Defaults to the entire SpatRaster.
-#' @param ext character, a graphic format extension.
 #' @param filename NULL (default) to write automatically into products, provide full path and ext to override.
+#' @param extension character, a graphic format extension.
 #'
 #' @return A filtered terra SpatRaster.
 #' @export
 #'
-filter_savgol <- function(raster, p = 3, n = p + 13 - p %% 2, m = 0, ts = 1, extent = NULL, ext = NULL, filename = NULL) {
+filter_savgol <- function(
+  raster,
+  p = 3,
+  n = p + 13 - p %% 2,
+  m = 0,
+  ts = 1,
+  extent = NULL,
+  filename = NULL,
+  extension = NULL
+) {
   # Check if correct class is supplied.
   if (!inherits(raster, what = "SpatRaster")) {
     rlang::abort(message = "Supplied data is not a terra SpatRaster.")
   }
 
-  # Raster source directory
-  raster_src <- raster |>
-    terra::sources() |>
-    fs::path_dir()
+  # Filename handling
+  if (is.null(filename)) {
+    # Extract source information
+    raster_src <- dirname(terra::sources(raster))
 
-  # Raster source name
-  raster_name <- raster |>
-    terra::sources() |>
-    fs::path_file() |>
-    fs::path_ext_remove()
+    # Extract file name
+    raster_name <- tools::file_path_sans_ext(basename(terra::sources(raster)))
 
-  # Check type of filename
-  if (is.null(filename) == TRUE) {
-    filename <- paste0(raster_src, "/", raster_name, "_SAVITZKY-GOLAY.tif")
+    # Construct default filename
+    filename <- file.path(
+      raster_src,
+      paste0(raster_name, "_SAVITZKY-GOLAY.tif")
+    )
   } else {
-    filename <- fs::path(filename, ext = ext)
+    # Ensure proper file extension is applied
+    filename <- fs::path(filename, extension = extension %||% "tif")
   }
 
-  # Check extent type
-  if (is.null(extent)) {
-    # Set window of interest
-    terra::window(raster) <- terra::ext(raster)
-  } else {
+  # Extent handling
+  if (!is.null(extent)) {
     # Set window of interest
     terra::window(raster) <- terra::ext(extent)
+  } else {
+    # Set window of interest
+    terra::window(raster) <- terra::ext(raster)
   }
 
-  # Named list with write options
-  wopts <- list(steps = terra::ncell(raster) * terra::nlyr(raster))
+  # # Raster source directory
+  # raster_src <- raster |>
+  #   terra::sources() |>
+  #   fs::path_dir()
+
+  # # Raster source name
+  # raster_name <- raster |>
+  #   terra::sources() |>
+  #   fs::path_file() |>
+  #   fs::path_ext_remove()
+
+  # # Check type of filename
+  # if (is.null(filename) == TRUE) {
+  #   filename <- paste0(raster_src, "/", raster_name, "_SAVITZKY-GOLAY.tif")
+  # } else {
+  #   filename <- fs::path(filename, ext = ext)
+  # }
+
+  # # Check extent type
+  # if (is.null(extent)) {
+  #   # Set window of interest
+  #   terra::window(raster) <- terra::ext(raster)
+  # } else {
+  #   # Set window of interest
+  #   terra::window(raster) <- terra::ext(extent)
+  # }
+
+  # Write options
+  wopts <- list(
+    steps = terra::ncell(raster) * terra::nlyr(raster),
+    overwrite = TRUE
+  )
 
   # Extract names
   band_names <- names(raster)
@@ -340,7 +379,6 @@ filter_savgol <- function(raster, p = 3, n = p + 13 - p %% 2, m = 0, ts = 1, ext
     raster,
     fun = \(raster) signal::sgolayfilt(raster, p = p, n = n, m = m, ts = ts),
     filename = filename,
-    overwrite = TRUE,
     wopt = wopts
   )
 
