54679541395493a27a5e9d8e9b9a7f352fe919b1
mzarowka <maurycy.zarczynski@outlook.com>
2025-08-20 18:37:16 +0200

    Tests reflectance values and layer number
    Ensures that the reflectance calculation produces the expected
    values. It also implicitly tests the correct number of layers
    in the SpatRaster objects used in the calculation.
    Fails devtools::check()

--- tests/testthat/test-hsi_transformations.R
+++ tests/testthat/test-hsi_transformations.R
@@ -1,28 +1,32 @@
-# Test reflectance calculation
+# Test reflectance calculation and values produced
+# In a way it also test for a number of layers in a SpatRaster
 test_that("reflectance is calculated as expected", {
   expect_equal(
-    hsi_reflectance(
+    terra::values(hsi_reflectance(
       sample = terra::rast(
-        fs::path_package("HSItools", "inst/testdata/capture/testdata.tif")
+        fs::path_package(
+          package = "HSItools",
+          "inst/testdata/capture/testdata.tif"
+        )
       ),
       whiteref = terra::rast(
         fs::path_package(
-          "HSItools",
+          package = "HSItools",
           "inst/testdata/capture/WHITEREF_testdata.tif"
         )
       ),
       darkref = terra::rast(
         fs::path_package(
-          "HSItools",
+          package = "HSItools",
           "inst/testdata/capture/DARKREF_testdata.tif"
         )
       )
-    ),
-    terra::rast(
+    )),
+    terra::values(terra::rast(
       fs::path_package(
-        "HSItools",
+        package = "HSItools",
         "inst/testdata/products/REFLECTANCE_testdata.tif"
       )
-    )
+    ))
   )
 })
